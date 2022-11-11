3ceebf941efab64fe30daa2f717fdc9bc4bd82e6
Sanjeev <53875709+Sanjeev-007@users.noreply.github.com>
2022-11-11 17:05:15 +0530

    Create variables.tf

---
+++ terraform/environments/master/variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+        default = "cryptic-ground-365404"
+    }
+variable "region" {
+    default = "us-west1"
+}
+variable "serverless_vpc_conn_cidr" {
+    default= "10.8.0.0/28"
+}
+variable "network" {
+    default= "defaults-nat"
+}
