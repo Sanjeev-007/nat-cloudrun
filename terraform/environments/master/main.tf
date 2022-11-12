6529d727419167a5e5ffa90f73cc5257c350b545
Sanjeev <53875709+Sanjeev-007@users.noreply.github.com>
2022-11-12 09:25:55 +0530

    Update main.tf

--- terraform/environments/master/main.tf
+++ terraform/environments/master/main.tf
@@ -74,8 +74,7 @@ resource "google_cloud_run_service" "gcr_service" {
         }
       }
       # the service uses this SA to call other Google Cloud APIs
-      #service_account_name = "191628563199-compute@developer.gserviceaccount.com"
-      service_account_name ="shared-vpc-serviceacc@giridhar-project-1.iam.gserviceaccount.com"
+      service_account_name = "191628563199-compute@developer.gserviceaccount.com"
     }
 
     metadata {
